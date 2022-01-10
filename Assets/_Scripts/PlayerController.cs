dad64e17f8bbc73793818a31423bd6cc6c5f6da0
DRicoT <wasnaisxp@gmail.com>
2022-01-10 21:06:23 +0100

    Ejercicio_138

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 movement;
+
+    private Animator _animator;
+    [SerializeField] private float turnSpeed = 20f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        movement.Set(horizontalInput, 0 , verticalInput);
+        movement.Normalize();
+
+        // Mathf.Approximately: Compara los parámetros y si uno se aproxima al otro, lo anula. Si horizontalInput se aproxima a 0, dará 0
+        // Con esto conseguimos que si por sensibilidad de las teclas, hubiera un valor que no sea 0 puro, por pequeñas vibraciones, lo cambia a 0
+        bool hasHorizontalInput = !Mathf.Approximately(horizontalInput, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(verticalInput, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        _animator.SetBool("isWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.deltaTime, 0f);
+    }
+}
